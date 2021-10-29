ef682d9dd5f30c34168f82be9ee8d5ae37615c53
Valeriu Rusu <valeriu.rusu@d-i-s.dk>
2021-10-29 14:21:14 +0200

    Sample request for creation of the template

--- components/Templates/Template.js
+++ components/Templates/Template.js
@@ -1,71 +1,87 @@
-import { BiSelectMultiple, BiCodeAlt } from "react-icons/bi";
+import { BiCodeAlt, BiSelectMultiple } from "react-icons/bi";
+
 import { BsQuestion } from "react-icons/bs";
+import Link from 'next/link'
 import { MdEmail } from "react-icons/md";
 
 function Template({ template }) {
   return (
-    <div className="bg-white  pt-4 pb-2 rounded-lg shadow-xl cursor-pointer ">
-      <div className="relative h-20 flex items-center">
-        <div className="absolute w-full flex justify-end mb-10 pr-4">
-          <p className="uppercase font-bold text-gray-400">{template.created}</p>
-        </div>
-        <div className="absolute h-0.5 w-full bg-gray-200"></div>
-        <div className="rounded-full h-16 w-16 white absolute ml-10 flex justify-center items-center">
-          <div className="p-2 rounded-full h-16 w-16 bg-white">
-            <img src="https://www.heyfunding.dk/images/logoer/stibo-accelerator.png" alt="logo" className="  " />
+    <Link href={`/templates/${template.id}`}>
+      <div className="bg-white pt-4 pb-2 rounded-lg shadow-xl cursor-pointer ">
+        <div className="relative h-20 flex items-center">
+          <div className="absolute w-full flex justify-end mb-10 pr-4">
+            <p className="uppercase font-bold text-gray-400">
+              {template.created}
+            </p>
+          </div>
+          <div className="absolute h-0.5 w-full bg-gray-200"></div>
+          <div className="rounded-full h-16 w-16 white absolute ml-10 flex justify-center items-center">
+            <div className="p-2 rounded-full h-16 w-16 bg-white">
+              <img
+                src="https://www.heyfunding.dk/images/logoer/stibo-accelerator.png"
+                alt="logo"
+                className="  "
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="pl-4 pr-2 mt-2 flex justify-between">
-        <div className="flex">
-          {template.multiple ? (
-            <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
-              <BiSelectMultiple className="h-6 w-6 text-white" />
-            </div>
-          ) : null}
-          {template.single ? (
-            <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1">
-              <BsQuestion className="h-6 w-6 text-white" />
-            </div>
-          ) : null}
+        <div className="pl-4 pr-2 mt-2 flex justify-between">
+          <div className="flex">
+            {template.multiple ? (
+              <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
+                <BiSelectMultiple className="h-6 w-6 text-white" />
+              </div>
+            ) : null}
+            {template.single ? (
+              <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1">
+                <BsQuestion className="h-6 w-6 text-white" />
+              </div>
+            ) : null}
 
-          {template.mail ? (
-            <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
-              <MdEmail className="h-6 w-6 text-white" />
-            </div>
-          ) : null}
-          {template.code ? (
-            <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
-              <BiCodeAlt className="h-6 w-6 text-white" />
-            </div>
-          ) : null}
+            {template.mail ? (
+              <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
+                <MdEmail className="h-6 w-6 text-white" />
+              </div>
+            ) : null}
+            {template.code ? (
+              <div className="rounded-full bg-blue-400 h-8 w-8 flex justify-center items-center border-2 border-blue-500 mr-1 p-1">
+                <BiCodeAlt className="h-6 w-6 text-white" />
+              </div>
+            ) : null}
+          </div>
+          <div className="flex justify-center items-center pr-2">
+            <p className="uppercase font-extrabold text-green-500 text-sm">{`${template.tasks} tasks`}</p>
+          </div>
         </div>
-        <div className="flex justify-center items-center pr-2">
-          <p className="uppercase font-extrabold text-green-500 text-sm">{`${template.tasks} tasks`}</p>
+
+        <div className="pl-4 pr-2 mt-4">
+          <p className="text-2xl font-extrabold text-gray-800 truncate">
+            {template.name}
+          </p>
+          <div className="mt-1 ">
+            <p className="text-sm text-gray-600 break-normal line-clamp-2 ...">
+              {template.description}{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="pl-4 pr-4 mt-4 flex justify-between items-center">
+          <div>
+            <p className="font-bold text-gray-700">{`87 candidates`}</p>
+          </div>
+          <div>
+            <p className="font-bold text-gray-700">{`Avg score: 80%`}</p>
+          </div>
+        </div>
+
+        <div className="pb-2 pt-4 flex justify-center items-center">
+          <button className="bg-gray-800 pr-2 pl-2 pb-1 pt-1 rounded-lg text-white font-semibold hover:bg-gray-700">
+            In Use
+          </button>
         </div>
       </div>
-
-      <div className="pl-4 pr-2 mt-4">
-        <p className="text-2xl font-extrabold text-gray-800 truncate">{template.name}</p>
-        <div className="mt-1 ">
-          <p className="text-sm text-gray-600 break-normal line-clamp-2 ...">{template.description} </p>
-        </div>
-      </div>
-
-      <div className="pl-4 pr-4 mt-4 flex justify-between items-center">
-        <div>
-          <p className="font-bold text-gray-700">{`87 candidates`}</p>
-        </div>
-        <div>
-          <p className="font-bold text-gray-700">{`Avg score: 80%`}</p>
-        </div>
-      </div>
-
-      <div className="pb-2 pt-4 flex justify-center items-center">
-        <button className="bg-gray-800 pr-2 pl-2 pb-1 pt-1 rounded-lg text-white font-semibold hover:bg-gray-700">In Use</button>
-      </div>
-    </div>
+    </Link>
   );
 }
 
---
+++ templates.js
@@ -0,0 +1,62 @@
+export const templates = [
+  {
+    id: 1,
+    name: "Solution Architect",
+    description:
+      "This a Solution Architect job description and it is not long. It will be great to get an experienced person on board which will have good interview results.",
+    created: "3 DAYS AGO",
+    multiple: true,
+    mail: true,
+    single: true,
+    code: false,
+    tasks: 6,
+  },
+  {
+    id: 2,
+    name: "Software Engineering",
+    description:
+      "This a Software Engineering job description and it is not long. It will be great to get an experienced person on board which will have good interview results.",
+    created: "5 DAYS AGO",
+    multiple: true,
+    mail: false,
+    single: false,
+    code: true,
+    tasks: 11,
+  },
+  {
+    id: 3,
+    name: "Backend-Developer",
+    description:
+      "This a Backend-Developer job description and it is not long. It will be great to get an experienced person on board which will have good interview results.",
+    created: "6 DAYS AGO",
+    multiple: true,
+    mail: false,
+    single: true,
+    code: true,
+    tasks: 13,
+  },
+  {
+    id: 4,
+    name: "Frontend-Developer",
+    description:
+      "This a Frontend-Developer job description and it is not long. It will be great to get an experienced person on board which will have good interview results.",
+    created: "17 DAYS AGO",
+    multiple: true,
+    mail: false,
+    single: true,
+    code: true,
+    tasks: 9,
+  },
+  {
+    id: 5,
+    name: "Project Manager",
+    description:
+      "This a Project Manager job description and it is not long. It will be great to get an experienced person on board which will have good interview results.",
+    created: "1 MONTH AGO",
+    multiple: false,
+    mail: true,
+    single: true,
+    code: false,
+    tasks: 7,
+  },
+];
