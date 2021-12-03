edbb5752e9a78eb75714d014f3398b7a3a6d27ea
David Le <ledaodavid@gmail.com>
2021-12-03 13:47:39 +0100

    Update [id].ts

--- pages/api/interview/[id].ts
+++ pages/api/interview/[id].ts
@@ -36,41 +36,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // const authValue = req.headers.authorization;
   const { order } = req.query;
   const body = req.body;
+  const { id } = req.query;
   await connectDB();
 
   if (req.method === "GET") {
     try {
       // var interviewId = verifyAuthValue(authValue);
-      await Candidate.findOne({ "interviews._id": body.interviewId }).exec(
-        async function (err, candidate) {
-          if (err) {
-            return res.status(404).json({ error: "No Candidate with the i" });
-          }
+      await Candidate.findOne({ "interviews._id": id }).exec(async function (
+        err,
+        candidate
+      ) {
+        if (err) {
+          return res
+            .status(404)
+            .json({ error: "No Candidate with the interview id." });
+        }
 
-          var foundInterview = (
-            candidate as ICandidateDocument
-          ).interviews.find(
-            (interview: ICandidateInterviewDocument) =>
-              interview._id === body.interviewId
-          );
-          if (!foundInterview) {
+        var foundInterview = (candidate as ICandidateDocument).interviews.find(
+          (interview: ICandidateInterviewDocument) => interview._id === id
+        );
+        if (!foundInterview) {
+          return res.status(404).json({ error: "No template found." });
+        }
+
+        await Template.findOne({ jobId: foundInterview.jobId }).exec(function (
+          err,
+          template
+        ) {
+          if (err) {
             return res.status(404).json({ error: "No template found." });
           }
-
-          await Template.findOne({ jobId: foundInterview.jobId }).exec(
-            function (err, template) {
-              if (err) {
-                return res.status(404).json({ error: "No template found." });
-              }
-              var task = (template as ITemplateDocument).tasks.find(
-                (item: ITaskDocument) =>
-                  item.order == parseInt(order as string) + 1
-              );
-              return res.status(200).json(task);
-            }
+          var task = (template as ITemplateDocument).tasks.find(
+            (item: ITaskDocument) => item.order == parseInt(order as string) + 1
           );
-        }
-      );
+          return res.status(200).json(task);
+        });
+      });
     } catch (error) {
       const result = handleError(error as Error);
       return res.status(result.code).json({ error: result.error });
@@ -82,7 +83,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // var interviewId = verifyAuthValue(authValue);
       let time = convertToTimeSpan(body.startedUtc, body.completedUtc);
       await Candidate.findOneAndUpdate(
-        { "interviews._id": body.interviewId },
+        { "interviews._id": id },
         {
           "interviews.$.startedUtc": body.startedUtc,
           "interviews.$.completedUtc": body.completedUtc,
