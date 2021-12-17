ddd4fbff3e8a4b3fa7eb7c0deef57643fb3d529a
David Le <ledaodavid@gmail.com>
2021-12-17 12:20:08 +0100

    Update candidate.test.js

--- tests/api/candidate.test.js
+++ tests/api/candidate.test.js
@@ -29,13 +29,56 @@ describe("POST: api/account/candidates", () => {
         { order: 10, answer: "TestAnswer11" },
       ],
     };
+
+    it("Should return 200 OK status with correct credentials", async () => {
+      const body = {
+        firstName: "TestFirstName",
+        lastName: "TestLastName",
+        email: "candidate@test.com",
+        jobId: "TestJobId",
+        answers: [
+          { order: 0, answer: "TestAnswer1" },
+          { order: 1, answer: "TestAnswer2" },
+          { order: 2, answer: "TestAnswer3" },
+          { order: 3, answer: "TestAnswer4" },
+          { order: 4, answer: "TestAnswer5" },
+          { order: 5, answer: "TestAnswer6" },
+          { order: 6, answer: "TestAnswer7" },
+          { order: 7, answer: "TestAnswer8" },
+          { order: 8, answer: "TestAnswer9" },
+          { order: 9, answer: "TestAnswer10" },
+          { order: 10, answer: "TestAnswer11" },
+        ],
+      };
+      await testClient(createHandler)
+        .post("/api/account/candidates")
+        .send(body)
+        .expect(201)
+        .expect("Content-Type", /json/)
+        .expect((response) => {
+          expect(response.body).toBeDefined();
+          console.log(response.body);
+        });
+    });
+
+  it("Should return 200 OK status with correct credentials", async () => {
+    const body = {
+      firstName: "TestFirstName",
+      lastName: "TestLastName",
+      email: "candidate@test.com",
+      jobId: "TestJobId",
+      answers: [{ order: 0, answer: "TestAnswer1" }],
+    };
     await testClient(createHandler)
       .post("/api/account/candidates")
       .send(body)
-      .expect(201)
+      .expect(400)
       .expect("Content-Type", /json/)
       .expect((response) => {
         expect(response.body).toBeDefined();
+        expect(response.body.error).toBe(
+          "answers field must have at least 10 items"
+        );
       });
   });
 });
