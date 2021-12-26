418de883d492f1c727a0c34f83bf67bfda632692
David Le <ledaodavid@gmail.com>
2021-12-26 21:07:22 +0100

    Create index.ts

---
+++ pages/api/positions/index.ts
@@ -0,0 +1,23 @@
+import handleError from "../../../helpers/errorHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import JobPosition from "../../../models/JobPosition";
+import connectDB from "../../../utils/mongodb";
+
+export default nextConnect().get(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      await connectDB();
+      let positions = await JobPosition.find({
+        companyId: "6182887f8a051eb01be80084",
+      })
+        .select("name")
+        .lean();
+
+      return res.status(200).json(positions);
+    } catch (error) {
+      const result = handleError(error as Error);
+      return res.status(result.code).json({ error: result.error });
+    }
+  }
+);
